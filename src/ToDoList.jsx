364e06ce2ec64431a72ffd247608d205ae2a9808
MuhammadNazim6 <nazimnazz66@gmail.com>
2024-02-16 14:52:33 +0530

    Major changes

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
-
+console.log(localStorage.getItem("myTodoLists"));
 function ToDoList() {
 
-  const [tasks, setTasks] = useState(['Take a shower', 'Complete the project', 'Book the flight ticket', 'Wash the car'])
+
+  const [tasks, setTasks] = useState(()=>{
+    const storedTasks = localStorage.getItem("myTodoLists")
+    return storedTasks ? JSON.parse(storedTasks) : []
+  })
+
   const [newTask, setNewtask] = useState()
 
   function handleInputTask(event) {
@@ -13,17 +18,25 @@ function ToDoList() {
   }
   function addTask() {
     if (newTask.trim() !== "") {
+      
       setTasks([...tasks, newTask])
+      localStorage.setItem("myTodoLists", JSON.stringify([...tasks, newTask]));
+      console.log(localStorage.getItem("myTodoLists"))
+
       setNewtask('')
+
     }
   }
   function deleteTask(index) {
     setTasks(tasks.filter((_, i) => i !== index))
+    localStorage.setItem("myTodoLists", JSON.stringify(tasks.filter((_, i) => i !== index)));
   }
   function moveTaskUp(index) {
     if (index !== 0) {
       [tasks[index], tasks[index - 1]] = [tasks[index - 1], tasks[index]]
       setTasks([...tasks])
+      localStorage.setItem("myTodoLists", JSON.stringify([...tasks]));
+
     }
 
   }
@@ -31,15 +44,15 @@ function ToDoList() {
     if (index !== tasks.length - 1) {
       [tasks[index], tasks[index + 1]] = [tasks[index + 1], tasks[index]]
       setTasks([...tasks])
+      localStorage.setItem("myTodoLists", JSON.stringify([...tasks]));
+
     }
   }
-
-
 
   return (
     <>
       <h2>To do list</h2>
-      <div class="center-div">
+      <div className="center-div">
         <input
           id="inputTag"
           type='text'
@@ -50,16 +63,16 @@ function ToDoList() {
 
         <button className="add-button" onClick={addTask}>Add Task</button>
       </div>
-      <div class="content-div">
+      <div className="content-div">
         <ol>
           
           { tasks.length > 0 ? 
             tasks.map((task, index) =>
               <li key={index}>
                 <span className='text'>  {task}  </span>
-                <button className='delete-button' onClick={() => deleteTask(index)}><i class="fa-solid fa-trash"></i></button>
-                <button className='move-button' onClick={() => moveTaskUp(index)}><i class="fa-solid fa-arrow-up"></i></button>
-                <button className='move-button' onClick={() => moveTaskDown(index)}><i class="fa-solid fa-arrow-down"></i></button>
+                <button className='delete-button' onClick={() => deleteTask(index)}><i className="fa-solid fa-trash"></i></button>
+                <button className='move-button' onClick={() => moveTaskUp(index)}><i className="fa-solid fa-arrow-up"></i></button>
+                <button className='move-button' onClick={() => moveTaskDown(index)}><i className="fa-solid fa-arrow-down"></i></button>
               </li>) : 
               <h3>No tasks available</h3>
           }
